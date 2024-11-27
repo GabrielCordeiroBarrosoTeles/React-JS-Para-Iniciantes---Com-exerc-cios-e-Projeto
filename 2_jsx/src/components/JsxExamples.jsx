b6ea1ef9118efe0810c1532628cd5a54b11511a5
Gabriel Cordeiro Barroso Teles <gabrielpikachuoficial@gmail.com>
2024-11-27 11:04:18 -0300

    aula 10

--- 2_jsx/src/components/JsxExamples.jsx
+++ 2_jsx/src/components/JsxExamples.jsx
@@ -11,6 +11,15 @@ const JsxExamples = () => {
       return `Olá ${name}`  
     }
 
+    const userIsLoggedIn = true;
+
+    const userRole = "admin";
+
+    const users = [
+        {id: 1, name: "Gabriel"},
+        {id: 2, name: "João"},
+        {id: 3, name: "Pedro"},
+    ]
   return (
     <div>
         <h2>Conteúdo que o usuáreio vai ver</h2>
@@ -18,6 +27,35 @@ const JsxExamples = () => {
         <p>O nome do usuário é {user.name} {user.lastName}</p>
         <p>{getGreeting(user.name)}</p>
 
+        {/* Diferenças do HTML*/}
+        <div className='teste'>
+            <button onClick={()=> alert("Teste")}>Clique em mim</button>   
+            <br/>
+            <br/>
+            <input type="text" placeholder='Digite algo' />
+
+            {/* Renderização Condicional */}
+
+            {userIsLoggedIn ? (
+                <p>Caso: está Logado</p>
+            ) : (
+                <p>Caso: não está Logado</p>
+            ) }
+
+            <p>{userRole === "admin" && "Você é um admin"}</p>
+
+            {/* Renderização de listas */}
+            <div>
+                <ul>
+                    {users.map((user) => (
+                        <li key={user.id}>
+                            {user.id} - {user.name}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        
+        </div>
     </div>
   )
 }
