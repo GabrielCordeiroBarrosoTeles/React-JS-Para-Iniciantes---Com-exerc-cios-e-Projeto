daa6b253e25ad771aa53362b786e11b449b098b9
Gabriel Cordeiro Barroso Teles <gabrielpikachuoficial@gmail.com>
2024-11-27 10:31:59 -0300

    Aula 8

--- 1_componentes/src/App.jsx
+++ 1_componentes/src/App.jsx
@@ -4,14 +4,18 @@ import './App.css'
 // Importar o componente filho
 import FunctionalComponent from './components/FunctionalComponent'
 import ClassComponent from './components/ClassComponent'
+import PropsExample from './components/PropsExample'
 
 function App() {
 
   return (
       <div>
         {/* Utilizando o componentre importado no JSX */}
+
         <FunctionalComponent />
         <ClassComponent />
+        <PropsExample nome = "Gabriel Cordeiro" idade = {19} />
+        
       </div>
   )
 }
