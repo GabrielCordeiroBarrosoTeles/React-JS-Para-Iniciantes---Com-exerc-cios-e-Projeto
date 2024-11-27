60ae8306b798f9929f5edfe576f356ad31a161af
Gabriel Cordeiro Barroso Teles <gabrielpikachuoficial@gmail.com>
2024-11-27 10:04:29 -0300

    Estudos em React

---
+++ 1_componentes/src/App.jsx
@@ -0,0 +1,16 @@
+// Componente Pai
+import './App.css'
+
+// Importar o componente filho
+import FunctionalComponent from './components/FunctionalComponent'
+
+function App() {
+
+  return (
+      <div>
+        <FunctionalComponent />
+      </div>
+  )
+}
+
+export default App
---
+++ 1_componentes/src/components/FunctionalComponent.jsx
@@ -0,0 +1,13 @@
+// Componente Filho
+
+// Cria um função
+function FunctionalComponent() {
+    // Retorna um objeto com jsx
+    return (
+        <div>
+            <h1>Opa meu amigo</h1>
+        </div>
+    )
+} 
+// Ele exporta todos os dados
+export default FunctionalComponent;
